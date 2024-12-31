7d9f1d6bba1a16b51e9bb8211f3e0c7873fad88b
PhamThanhSJU <146055328+PhamThanhSJU@users.noreply.github.com>
2024-12-31 17:04:02 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,12 +5,12 @@ import numpy as np
 import streamlit as st
 # Information
 st.sidebar.markdown("## Authors' information")
-st.sidebar.markdown("Authors: Van-Thanh Pham and Jong-Sung Kim")
-st.sidebar.caption("Department of Quantum and Nuclear Engineering, Sejong University, Korea")
-st.sidebar.caption("Emails: phamthanhwru@gmail.com and kimjsbat@sejong.ac.kr")
+st.sidebar.markdown("Authors: Thai-Hoang Nguyen,Van-Thanh Pham, Quang-Viet Vu, and Viet-Ngoc Pham")
+st.sidebar.caption("Faculty of Civil Engineering, Thuyloi University, Vietnam")
+st.sidebar.caption("Emails: phamthanhwru@gmail.com (V-T Pham")
 
 #Tên bài báo
-st.title ("Hybrid machine learning model using HHO algorithm for predicting the onset of void swelling in irradiated metals") 
+st.title ("Hybrid machine learning with FHO algorithm and WERCS method for predicting fire resistance of timber columns") 
 
 # Chèn sơ đồ nghiên cứu
 # st.header("1. Layout of this study")
@@ -31,47 +31,28 @@ st.title ("Hybrid machine learning model using HHO algorithm for predicting the 
 #    st.image("Fig2.jpg", caption="Overview on structure of Random Forest model") 
 	
 #Make a prediction
-st.header("Predicting the onset of void swelling in irradiated metals")
+st.header("Predicting the fire resistance of timber columns")
 st.subheader("Input variables")
-col1, col2, col3, col4, col5, col6 =st.columns(6)
+col1, col2, col3, col4 =st.columns(4)
 with col1:
-   X1=st.slider("Fe (wt. %)", 0.00, 97.00)
-   X2 = st.slider("Cr (wt. %)", 0.00, 24.70)
-   X3= st.slider("Mn (wt. %)", 0.00, 20.00)
-   X4 = st.slider("Si (wt. %)", 0.00, 1.50)
-   X5 = st.slider("Co /100 (wt. %)", 0.00, 4.00)
+   X1 = st.slider("D (mm)", 75.00, 400.00)
+   X2 = st.slider("W (mm)", 75.00, 400.00)
 	
 with col2:	
 
-   X6= st.slider("Mo (wt. %)", 0.00, 2.95)
-   X7 = st.slider("Ni (wt. %)", 0.00, 100.00)
-   X8=st.slider("C (wt. %)", 0.00, 1.00)	
-   X9 = st.slider("Ti (wt. %)", 0.0, 2.20)
-   X10= st.slider("N (wt. %)", 0.00, 0.30)
+   X3= st.slider("Density (kg/m3)", 310.00, 590.00)
+   X4 = st.slider("Compre. stre. (MPa)", 7.90, 62.00)
 
 with col3:		
-   X11 = st.slider("B /1000 (wt. %)", 0.00, 4.00)
-   X12 = st.slider("P /100 (wt. %)", 0.00, 15.50)
-   X13= st.slider("S /100 (wt. %)", 0.00, 3.00)
-   X14 = st.slider("Nb (wt. %)", 0.00, 0.92)   
-   X15=st.slider("Cu (wt. %)", 0.00, 0.54)	
+   X5 = st.slider("E (MPa)", 6895.00, 18616.00)
+   X6 = st.slider("L (mm)", 1800.00, 3658.00)
+   	
 	
 with col4:	
-   X16 = st.slider("Ta (wt. %)", 0.00, 0.36)
-   X17= st.slider("Al (wt. %)", 0.00, 100.00)
-   X18 = st.slider("V (wt. %)", 0.00, 2.00)
-   X19 = st.slider("Mg (wt. %)", 0.00, 1.63)
-   X20= st.slider("W (wt. %)", 0.00, 2.40)	
+   X7 = st.slider("C (kN)", 106.00, 5592.00)
+   X8 = st.slider("P (%)", 9.10, 60.00)
 
-with col5:	
-   X21 = st.slider("Zr (wt. %)", 0.00, 0.10)  
-   X22=st.slider("Dose rate /1000 (dpa/s)", 0.000, 60.00)
-   X23 = st.slider("Temperature (K)", 393.5, 1013.5)
-   X24= st.slider("Irradiation type [1-4]", 1, 4)
-	
-with col6:	
-   X25 = st.slider("Dislocation density x10^14 (m−2)", 0.30, 38.50)
-   X26 = st.slider("Pre-injected He (appm)", 0.00, 100.00)
+
 
 
 from sklearn.model_selection import train_test_split, KFold
@@ -84,23 +65,26 @@ from catboost import CatBoostRegressor
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-df = pd.read_csv('Swelling_data_Thanh.csv')
-X_ori = df[['Fe','Cr','Mn','Si','Co','Mo','Ni','C','Ti','N','B','P','S','Nb','Cu','Ta','Al','V','Mg','W','Zr','dr','tem','irrtype','disden','PreHe']].values
-y = df['dose'].values
-X = scaler.fit_transform(X_ori)	
+
+df = pd.read_csv('Timber_Thanh_WERCS.csv')
+X_ori = df[['D','W','den','fc','E','L','C','P']].values
+y = df['R'].values
+
+feature_name = ['D','W',r'$\rho$',r'$f_{c}$','E','L','C','P']
+
 X_train=X
 y_train=y
 
-n_estimators = 115
-learning_rate = 0.518
-depth = 9
-l2_leaf_reg = 9
+n_estimators = 180
+learning_rate = 0.225
+depth = 6
+l2_leaf_reg = 8
         
 cat_clf_n = CatBoostRegressor(n_estimators =n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
 cat_clf_n.fit(X_train, y_train)
 
 
-Inputdata = [X1, X2, X3, X4, X5/100, X6, X7,X8, X9, X10, X11/1000, X12/100, X13/100, X14, X15, X16, X17,X18, X19, X20, X21, X22/1000, X23, X24, X25*(10**14),X26]
+Inputdata = [X1, X2, X3, X4, X5, X6, X7,X8]
 
 
 from numpy import asarray
@@ -121,5 +105,5 @@ if st.button("Predict"):
        my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
-    st.success(f"Your predicted incubation dose (dpa) obtained from HHO-CGB model is: {(fc_pred2)}")
+    st.success(f"Your predicted fire resistance (minutes) obtained from FHO-CGB model is: {(fc_pred2)}")
 
